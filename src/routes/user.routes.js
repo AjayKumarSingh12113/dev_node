38f51fec01f08602904ec94874c932dc8af94183
AjayKumarSingh12113 <ajaykushwaha12113@gmail.com>
2025-08-02 14:17:33 +0530

    refactor user routes to include change password, current user, update account, avatar, cover image, user channel profile, and watch history functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    changeCurrentPassword,
+     getCurrectUser,
+      getUserChannelProfile, 
+      getWatchHistory,
+       loginUser,
+        logoutUser, 
+        refreshAccessToken, 
+        registerUser, 
+        updateAccountDetails, 
+        updateAvatar, 
+        updateCoverImage 
+    } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,4 +37,17 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT,getCurrectUser);
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"), updateCoverImage);
+    
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+
+router.route("/history").get(verifyJWT,getWatchHistory);
 export default router;
